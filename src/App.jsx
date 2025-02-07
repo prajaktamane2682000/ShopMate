8237bf36beeb19c17bb6817cf7c785912e42caa2
Prajakta Mane <prajaktamane2682000@gmail.com>
2025-02-07 23:42:29 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { AllRoutes } from "./Routes/AllRoutes";
+import { Header } from "./Components";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <AllRoutes />
+    </div>
+  );
+}
+
+export default App;
